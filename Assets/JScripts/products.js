8374721b50ba48da4216b287839900ce6cdc62b8
Yash Dhruwanshi <155844500+C-ninjaz@users.noreply.github.com>
2025-03-18 15:36:46 +0530

    Add files via upload

---
+++ Assets/JScripts/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Spinach",
+    description:
+      "Fresh and organic spinach. Packed with iron and essential nutrients.",
+    price: 50,
+    image: "/Assets/Images/elianna-friedman-4jpNPu7IW8k-unsplash.jpg",
+  },
+  {
+    name: "Brocolli",
+    description:
+      "Nutritious and delicious brocolli. Packed with vitamins and minerals.",
+    price: 199,
+    image: "/Assets/Images/annie-spratt-m1t-RJ1iCIU-unsplash.jpg",
+  },
+  {
+    name: "Potatoes",
+    description:
+      "Fresh and organic potatoes. Great for roasting, mashing, or frying.",
+    price: 40,
+    image: "/Assets/Images/rodrigo-dos-reis-h3AkzboxK4Q-unsplash.jpg",
+  },
+  {
+    name: "Olive Oil",
+    description:
+      "Extra virgin olive oil from Italy. Rich in antioxidants and healthy fats.",
+    price: 399,
+    image: "/Assets/Images/roberta-sorge-uOBApnN_K7w-unsplash.jpg",
+  },
+  {
+    name: "Tomatoes",
+    description:
+      "Fresh and juicy tomatoes. Perfect for salads, sauces, and sandwiches.",
+    price: 49,
+    image: "/Assets/Images/mae-mu-tbm6k2G-vAA-unsplash.jpg",
+  },
+  {
+    name: "Almonds",
+    description:
+      "Raw almonds from California. Packed with protein, fiber, and healthy fats.",
+    price: 99,
+    image: "/Assets/Images/chuttersnap-PYBmNk304G4-unsplash.jpg",
+  },
+  {
+    name: "Toor Dal",
+    description:
+      "High-quality toor dal from India. Rich in protein and essential nutrients.",
+    price: 119,
+    image: "/Assets/Images/chandan-chaurasia-J5KMugvdP44-unsplash.jpg",
+  },
+  {
+    name: "Cucumber",
+    description:
+      "Fresh and crisp cucumbers. Great for salads, pickles, and smoothies.",
+    price: 69,
+    image: "/Assets/Images/harshal-s-hirve-2GiRcLP_jkI-unsplash.jpg",
+  },
+];
